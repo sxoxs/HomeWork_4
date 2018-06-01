1862107b9ecc93f76436ecabe1ae51b1580f50df
sxoxs <33289095+sxoxs@users.noreply.github.com>
2018-06-01 17:31:33 +0300

    1. добавлен запуск рабочего варианта змейки

--- src/main/java/logik/Menu.java
+++ src/main/java/logik/Menu.java
@@ -8,11 +8,29 @@ import java.util.Scanner;
 
 public class Menu {
     public void showMenu() {
-        System.out.println("Добро пожаловать в игру крестики нолики!");
+        System.out.println("Добро пожаловать!");
         Scanner scanner = new Scanner(System.in);
-        TicTacToe ticTacToe = new TicTacToe();
         for (;;) {
-            ticTacToe.runGame();
+            System.out.println("Выберите игру:");
+            System.out.println("1. Крестики нолики");
+            System.out.println("2. Змейка");
+
+            switch (scanner.nextInt()){
+                case 1:{
+                    TicTacToe ticTacToe = new TicTacToe();
+                    ticTacToe.runGame();
+                    break;
+                }
+                case 2: {
+                    SnakeGame snakeGame = new SnakeGame();
+                    snakeGame.RunGame();
+                    break;
+                }
+                default:{
+                    System.out.println("Повторите выбор, вы что то не то нажали");
+                }
+            }
+
             System.out.println("Сыграть ещё?");
             System.out.println(" 1. да \n 2. нет");
             if (1 != scanner.nextInt()) {
@@ -20,9 +38,5 @@ public class Menu {
             }
         }
         System.out.println("Спасибо за игру!");
-    }
-    public void showSnake() throws IOException {
-        SnakeGame snakeGame = new SnakeGame();
-        snakeGame.RunGame();
     }
 }
